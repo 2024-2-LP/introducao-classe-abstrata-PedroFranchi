cb0f02b44931980a782c17526c4ca1a103cc67e3
Pedro <pedro.fjesus@sptech.school>
2024-11-07 18:28:03 -0300

    Atividade completa

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,33 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+    private Double raio;
+
+    public Circulo(String cor, Integer espessura, Double raio) {
+        super(cor, espessura);
+        this.raio = raio;
+    }
+
+    public Circulo() {
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                '}';
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,50 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
+
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaDasAreas = 0.0;
+        for (Figura figura : figuras) {
+            somaDasAreas += figura.calcularArea();
+        }
+        return somaDasAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasmaior20 = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20){
+                figurasmaior20.add(figura);
+            }
+        }
+        return figurasmaior20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> figurasQuadarado = new ArrayList<>();
+        for (Figura figura : figuras) {
+           if (figura instanceof Quadrado){
+               figurasQuadarado.add(figura);
+            }
+        }
+        return figurasQuadarado;
+    }
+
 }
